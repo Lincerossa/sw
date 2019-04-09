6631f4fc2b08af04a483c8d8be20ce3e1899fc86
Lincerossa90 <marcello.luatti@alkemy.com>
2019-04-09 16:36:20 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
 import slide1 from './slide_1.png'
+import slide2 from './slide_2.png'
 class App extends Component {
 
   render() {
     return (
       <div>
+        cachata
         <img src={slide1} />
+        non cachata
+        <img src={slide2} />
       </div>
     )
   }
